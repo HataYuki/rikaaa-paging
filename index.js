a0771a77b7119aceb331a3f22ca197a86c4dac8d
hata yuki <>
2019-09-26 21:18:55 +0900

    ライセンスの表記ミス

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  *
  *
  *
- * Copyright [2019] [rikaaa.org | Yuki Hata]
+ * Copyright 2019 rikaaa.org | Yuki Hata
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
