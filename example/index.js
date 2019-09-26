ad2942d306ae5544a8c4ef90004e8a3c4cf44a33
hata yuki <>
2019-09-27 08:50:21 +0900

    fix_#18の変更を取り込みたいので一旦コミット。exampleを作成中。

--- example/index.js
+++ example/index.js
@@ -1,6 +1,38 @@
 window.onload = function() {
-  var paging = self.rikaaaPaging(
-    ["#header", "#article", "#footer"],
-    this.document.querySelectorAll("a")
-  );
+  var ids = ["#header", "#article", "#footer"];
+
+  var paging = self.rikaaaPaging(ids, this.document.querySelectorAll("a"));
+
+  var header = this.document.querySelector("header");
+  var article = this.document.querySelector("article");
+  var footer = this.document.querySelector("footer");
+
+  paging.hookStart(function(data) {
+    var each = 500;
+    var duration = each * 3;
+
+    data.afterDelay = duration;
+
+    for (var i = 0; i < ids.length; i++) {}
+
+    data.onDelay = function(ratio) {
+      var step = Math.floor((ratio * duration) / each);
+
+      switch (step) {
+        case 0:
+          header.style.transition = "opacity 200ms ease-in-out";
+          header.style.opacity = 0;
+          break;
+        case 1:
+          article.style.transition = "opacity 200ms ease-in-out";
+          article.style.opacity = 0;
+          break;
+        case 2:
+          footer.style.transition = "opacity 200ms ease-in-out";
+          footer.style.opacity = 0;
+          break;
+      }
+    };
+    return data;
+  });
 };
