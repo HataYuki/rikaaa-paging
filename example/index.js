ea2a9cd5cf0859604597103512e3420ec2c03330
hata yuki <>
2019-09-27 09:15:14 +0900

    なんとなくexampleを実装。

--- example/index.js
+++ example/index.js
@@ -3,17 +3,17 @@ window.onload = function() {
 
   var paging = self.rikaaaPaging(ids, this.document.querySelectorAll("a"));
 
-  var header = this.document.querySelector("header");
-  var article = this.document.querySelector("article");
-  var footer = this.document.querySelector("footer");
+  var each = 200;
+  var duration = each * 3;
 
   paging.hookStart(function(data) {
-    var each = 500;
-    var duration = each * 3;
+    var header = this.document.querySelector(ids[0]),
+      article = this.document.querySelector(ids[1]),
+      footer = this.document.querySelector(ids[2]);
 
     data.afterDelay = duration;
 
-    for (var i = 0; i < ids.length; i++) {}
+    for (var i = 0; i < ids.length; i++) data.targets[ids[i]].style.opacity = 0;
 
     data.onDelay = function(ratio) {
       var step = Math.floor((ratio * duration) / each);
@@ -30,6 +30,32 @@ window.onload = function() {
         case 2:
           footer.style.transition = "opacity 200ms ease-in-out";
           footer.style.opacity = 0;
+          break;
+      }
+    };
+    return data;
+  });
+  paging.hookEnd(function(data) {
+    data.afterDelay = duration;
+
+    data.onDelay = function(ratio) {
+      var step = Math.floor((ratio * duration) / each);
+
+      switch (step) {
+        case 0:
+          data.updatedTargets[ids[step]].style.transition =
+            "opacity 200ms ease-in-out";
+          data.updatedTargets[ids[step]].style.opacity = 1;
+          break;
+        case 1:
+          data.updatedTargets[ids[step]].style.transition =
+            "opacity 200ms ease-in-out";
+          data.updatedTargets[ids[step]].style.opacity = 1;
+          break;
+        case 2:
+          data.updatedTargets[ids[step]].style.transition =
+            "opacity 200ms ease-in-out";
+          data.updatedTargets[ids[step]].style.opacity = 1;
           break;
       }
     };
